3a85ac6a27770b58e1ad35689b9dcc956b12a877
Lars Olesen <lars@intraface.dk>
2011-06-13 22:14:18 +0200

    Changed trigger_error() to throwing Exceptions

--- src/Ilib/DBQuery.php
+++ src/Ilib/DBQuery.php
@@ -375,8 +375,6 @@ class Ilib_DBQuery
             $url = "";
         }
 
-        // print($this->recordset_num_rows.' <= '.$this->rows_pr_page);
-
         if ($this->recordset_num_rows <= $this->rows_pr_page) {
             // Der er færre poster end pr. side. Paging kan ikke betale sig
             return array();
@@ -708,7 +706,7 @@ class Ilib_DBQuery
     public function useStored($value = true)
     {
         if (!in_array($value, array(true, false))) {
-            trigger_error("Første parameter til DBQuery->useStored() er ikke ente true eller false", E_USER_ERROR);
+            throw new Exception("Første parameter til DBQuery->useStored() er ikke ente true eller false");
         }
 
         $this->use_stored = $value;
@@ -719,10 +717,8 @@ class Ilib_DBQuery
      */
     public function loadStored()
     {
-        // $this->store_name != "" && (isset($_GET[$this->store_var_name]) && $_GET[$this->store_var_name] == "true")
         if ($this->store_session_id == '') {
-            trigger_error('You need to set session_id before storing a result. Use createStore to do that', E_USER_ERROR);
-            return false;
+            throw new Exception('You need to set session_id before storing a result. Use createStore to do that');
         }
 
         if (count($this->store_extra_condition) > 0) {
@@ -747,9 +743,6 @@ class Ilib_DBQuery
             }
             $this->sorting = unserialize(base64_decode($db->f("sorting")));
             $this->filter  = unserialize(base64_decode($db->f("filter")));
-            // $this->group_by = unserialize(base64_decode($db->f("group_by")));
-            // $this->having_condition = unserialize(base64_decode($db->f("having_condition")));
-
 
             $this->stored_character = $db->f("first_character");
             // Hvis character er sat, så benyttes character
@@ -791,7 +784,7 @@ class Ilib_DBQuery
         if ($use != "full" && $this->use_character) {
 
             if ($this->character_var_name == "") {
-                trigger_error("For at benytte useCharacter(), skal du også benytte defineCharacter()", E_USER_ERROR);
+                throw new Exception("For at benytte useCharacter(), skal du også benytte defineCharacter()");
             }
 
             if (isset($_GET[$this->character_var_name]) && $_GET[$this->character_var_name] != "") {
@@ -853,7 +846,9 @@ class Ilib_DBQuery
         // Tjekker antallet af poster for at se om character er nødvendigt!
         if ($csql != "") {
             $db->query("SELECT COUNT(".$this->table.".id) AS num_rows ".$sql.$sql_end);
-            $db->nextRecord() OR trigger_error("Kunne ikke eksekvere SQL-sætning", E_USER_ERROR);
+            if (!$db->nextRecord()) {
+                throw new Exception("Kunne ikke eksekvere SQL-sætning");
+            }
 
             if ($db->f("num_rows") > $this->rows_pr_page) {
                 $sql .= $csql; // tilføjer charater
@@ -870,7 +865,7 @@ class Ilib_DBQuery
         if ($use != "full" && $this->paging_var_name != "") {
 
             $db->query("SELECT COUNT(DISTINCT(".$this->table.".id)) AS num_rows ".$sql);
-            if ($db->nextRecord()) { // Dette er vist lige lovlig dræstisk: OR trigger_error("Kunne ikke eksekvere SQL-sætning", E_USER_ERROR);
+            if ($db->nextRecord()) {
                 $this->recordset_num_rows = $db->f("num_rows");
             } else {
                 $this->recordset_num_rows = 0;
@@ -886,8 +881,7 @@ class Ilib_DBQuery
         if ($use != "full" && $this->store_name != "") {
 
             if ($this->store_session_id == '') {
-                trigger_error('You need to set session_id before storing a result. Use createStore to do that', E_USER_ERROR);
-                return false;
+                throw new Exception('You need to set session_id before storing a result. Use createStore to do that');
             }
 
             if (count($this->store_extra_condition) > 0) {
@@ -907,10 +901,6 @@ class Ilib_DBQuery
                 sorting = \"".base64_encode(serialize($this->sorting))."\",
                 filter = \"".base64_encode(serialize($this->filter))."\",
                 date_time = NOW()";
-
-                // group_by = \"".base64_encode(serialize($this->group_by))."\",
-                // having_condition = \"".base64_encode(serialize($this->having_condition))."\",
-
 
             if ($this->store_toplevel == 1) {
                 $db->query("SELECT id FROM dbquery_result WHERE session_id = \"".$this->store_session_id."\" AND ".$store_extra_condition." toplevel = 1");
@@ -959,7 +949,7 @@ class Ilib_DBQuery
 
         if ($this->error->isError()) {
             // Hvis der er fejl returnere den default streng
-            // Den skal ikke trigger error. Det giver problemer hvis der er fejl i betaling af faktura. Men hvad skal den så? /Sune (29/6 2005)
+            // @todo Den skal ikke trigger error. Det giver problemer hvis der er fejl i betaling af faktura. Men hvad skal den så? /Sune (29/6 2005)
             //trigger_error("Der er opstået en fejl i dbquery->getSqlString()", ERROR);
         }
 
